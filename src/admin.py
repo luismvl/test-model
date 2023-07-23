b156df899cd044a6e8aeb0eb1821ad5c4796bbac
Luis Vela <30742037+luismvl@users.noreply.github.com>
2023-07-23 03:29:55 +0000

    remove comments

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User, Product, Size
+from models import db, User, Product, Size, ProductSizesQuantity
 from flask_admin.contrib.sqla import ModelView
 
 
@@ -13,6 +13,7 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Product, db.session))
     admin.add_view(ModelView(Size, db.session))
+    admin.add_view(ModelView(ProductSizesQuantity, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -17,10 +17,11 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
         }
+
 class ProductSizesQuantity(db.Model):
     __tablename__= 'product_sizes_quantity'
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
-    size_id = db.Column(db.Integer, db.ForeignKey('sizes.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), primary_key=True)
+    size_id = db.Column(db.Integer, db.ForeignKey('sizes.id'), primary_key=True)
     quantity = db.Column(db.Integer, nullable=False, default=0)
 
     __table_args__ = (db.UniqueConstraint('product_id', 'size_id', name='product_size_unique'),)
